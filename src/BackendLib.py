e85700a0f1e6ebc7bbddc47772c9ec3a11bcb265
lolwhat-codesucc <104655370+lolwhat-codesucc@users.noreply.github.com>
2023-04-15 20:48:26 +0500

    Add files via upload

--- src/BackendLib.py
+++ src/BackendLib.py
@@ -9,12 +9,26 @@ class Meta:
         return self.description
     def getName(self):
         return self.name
-class Test(Meta):
-    def __init__(self,meta: Meta,):
-        pass
 
+class Task(Meta):
+    def __init__(taskType: str, meta: Meta, expectedResult: str):
+        self.type = taskType
+        self.meta = meta
+        self.expectedResult = expectedResult
+    def getType(self):
+        return self.type
+    def getExpectedResult(self):
+        return self.expectedResult
+
+class Test(Meta):
+    def __init__(self,meta: Meta,*tasks):
+        self.meta = meta
+        self.tasks = tasks
+    def getTaskSet(self):
+        return self.tasks
+        
 class Solution:
-    def __init__(self,task: Task, code: str = None, compInfo:str = None):
-        self.compInfo = compInfo
-        self.code = code
-        self.task = task
+    def __init__(self,taskId:int, taskType:str, result:str):
+        self.taskId = taskId
+        self.taskType = taskType
+        self.result = result
